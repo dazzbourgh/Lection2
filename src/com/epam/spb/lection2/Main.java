f2280bba24d671d1e85bc16cc0aef9b8d3f2614c
Leonid Borisevich <leonidborisevich00@gmail.com>
2016-07-02 00:50:25 +0300

    Wrote code for Main.main(). Wanted to add instances of all classes to a list and then run required instance if it corresponds to argument, but class "Object" does not have main(), so it didn't work. TODO: find out if it's possible to make it that way.

--- src/com/epam/spb/lection2/Main.java
+++ src/com/epam/spb/lection2/Main.java
@@ -1,13 +1,54 @@
 package com.epam.spb.lection2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+    //public enum NAME {ARGUMENTS_EXAMPLE};
 
     public Main() {
 
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello guys!");
+       // List<Integer> list = new ArrayList<Integer>();
+        List<Object> list1 = new ArrayList<Object>();
+        list1.add(new ArgumentsExample());
+        list1.add(new AutoPack());
+        list1.add(new BigExample());
+        list1.add(new BitOperations());
+        list1.add(new CharsTest());
+        list1.add(new ClassExample());
+        list1.add(new CoverClasses());
+        list1.add(new DataTypes());
+        list1.add(new Import());
+        list1.add(new InstanceOf());
+        list1.add(new LogicOperations());
+        list1.add(new Operators());
+        list1.add(new StringTransformation());
+        list1.add(new UnaryOperators());
+        String inputStr = "";
+        int input = 0;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Hello guys!");
+        System.out.println("Enter number of subroutine: ");
+        inputStr = scanner.nextLine();
+        try {
+            input = Integer.getInteger(inputStr);
+        }
+        catch (Exception e){
+            if (inputStr.equals("quit") == false)
+                System.out.println("Input error");
+            return;
+        }
+        System.out.println(input);
+        for(Object val : list1){
+            if(val.getClass().equals(input)){
+        //        val.main("Some arguments");
+            }
+        }
     }
 }
